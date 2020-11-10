b4760da36554d0052da0a70733c01375a5190602
edibeozkul <>
2020-11-10 15:25:21 +0300

    LogIn Page
    LogIn page is done.

---
+++ pages/inc-functions.php
@@ -0,0 +1,22 @@
+<?php 
+// Veritabanı bağlantısı
+header("Content-Type: text/html; charset=utf-8");
+// error_reporting(0);
+setlocale(LC_ALL, 'tr_TR.UTF-8', 'tr_TR', 'tr', 'turkish');
+
+$DBhost = "localhost";
+$DBuser = "root";
+$DBpass = "";
+$DBname = "yazilimbakimi";
+
+try {
+    $db = new PDO("mysql:host=$DBhost; dbname=$DBname", $DBuser, $DBpass);
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+$db->exec("SET NAMES 'utf8'; SET CHARSET 'utf8'; ");
+
+define("_URL", "http://localhost/yazilimbakimi/");
+  
+?>
---
+++ pages/inc-menu.php
@@ -0,0 +1,38 @@
+<?php 
+session_start();
+if(@$_SESSION["girisKontrol"] != 1) {
+    header("Location: index.php?i=hack");
+}
+?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html">Panel</a>
+    </div>
+    <!-- /.navbar-header -->
+
+    <ul class="nav navbar-top-links navbar-right">
+               
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-user">
+                <li><a href="cikis.php"><i class="fa fa-sign-out fa-fw"></i> LogOut</a>
+                </li>
+            </ul>
+            <!-- /.dropdown-user -->
+        </li>
+        <!-- /.dropdown -->
+    </ul>
+    <!-- /.navbar-top-links -->
+
+    <!-- /.navbar-static-side -->
+</nav>
