a3ec3992871cfe62a02230bca5124d5a00f61c72
Sadullah GOVENC <48141225+SadullahGovenc@users.noreply.github.com>
2020-11-17 13:36:57 +0300

    Update inc-menu.php
    Sol bar işlevleri tamamlandı.

--- pages/inc-menu.php
+++ pages/inc-menu.php
@@ -17,22 +17,49 @@ if(@$_SESSION["girisKontrol"] != 1) {
         <a class="navbar-brand" href="index.html">Panel</a>
     </div>
     <!-- /.navbar-header -->
-
     <ul class="nav navbar-top-links navbar-right">
                
-        <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-            </a>
-            <ul class="dropdown-menu dropdown-user">
-                <li><a href="cikis.php"><i class="fa fa-sign-out fa-fw"></i> LogOut</a>
-                </li>
-            </ul>
-            <!-- /.dropdown-user -->
-        </li>
-        <!-- /.dropdown -->
-    </ul>
-    <!-- /.navbar-top-links -->
-
-    <!-- /.navbar-static-side -->
+               <li class="dropdown">
+                   <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                       <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                   </a>
+                   <ul class="dropdown-menu dropdown-user">
+                       <li><a href="cikis.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                       </li>
+                   </ul>
+                   <!-- /.dropdown-user -->
+               </li>
+               <!-- /.dropdown -->
+           </ul>
+           <!-- /.navbar-top-links -->
+       
+           <div class="navbar-default sidebar" role="navigation">
+               <div class="sidebar-nav navbar-collapse">
+                   <ul class="nav" id="side-menu">
+                       <li class="sidebar-search">
+                           <div class="input-group custom-search-form">
+                               <input type="text" class="form-control" placeholder="Search...">
+                               <span class="input-group-btn">
+                               <button class="btn btn-default" type="button">
+                                   <i class="fa fa-search"></i>
+                               </button>
+                           </span>
+                           </div>
+                           <!-- /input-group -->
+                       </li>
+                       <li>
+                           <a href="dashboard.php"><i class="fa fa-dashboard fa-fw"></i> Anasayfa</a>
+                       </li>
+                       <li>
+                           <a href="blog.php"><i class="fa fa-table fa-fw"></i> Blog Yazıları</a>
+                       </li>
+                       <li>
+                           <a href="hakkimizda.php"><i class="fa fa-table fa-fw"></i> Hakkımızda</a>
+                       </li>
+                     
+                   </ul>
+               </div>
+               <!-- /.sidebar-collapse -->
+           </div>
+           <!-- /.navbar-static-side -->
 </nav>
